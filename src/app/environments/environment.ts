d192f5007fa7f58e3a80b79125f021e474aa4807
Nimish <nimishkannan62@gmail.com>
2025-03-26 00:14:29 +0530

    baseurl changed

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_ENDPOINT: "http://localhost:3000"
+    API_ENDPOINT: "https://e29f-2402-3a80-4481-93b4-d026-7b4f-b4fb-8801.ngrok-free.app"
 };
